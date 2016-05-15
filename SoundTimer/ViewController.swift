25df6963547db7741060c503ef4c9582a1e97b26
Peter Vorwieger <peter.vorwieger@me.com>
2016-05-15 22:03:35 +0200

    Initial Commit

---
+++ SoundTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundTimer
+//
+//  Created by Peter Vorwieger on 15.05.16.
+//  Copyright © 2016 Peter Vorwieger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
